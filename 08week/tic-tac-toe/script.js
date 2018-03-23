17a762f597df9a040149fb76eef5507f13c5fe78
Tim <timcheek99@gmail.com>
2018-03-22 19:03:08 -0500

    added js code to make a tic tac toe game still in progress..

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,62 @@
 'use strict';
 
-$(document).ready(function() {
+$(document).ready(function(){
   // Put app logic in here
+  var turn = 'X';
+  $('[data-cell]').on('click', function(){
+    if($(this).text() === ''){
+      $(this).text(turn);
+        wins();
+        if(turn === 'O'){
+          turn = 'X';
+
+        }else{
+          turn = 'O';
+
+        }
+      }
+  })
+
+  function wins() {
+    if($('[data-cell="0"]').text() === turn &&
+      $('[data-cell="3"]').text() === turn &&
+      $('[data-cell="6"]').text() === turn ){
+      alert(`${turn} Wins!`);
+    }
+      if($('[data-cell="1"]').text() === turn &&
+        $('[data-cell="4"]').text() === turn &&
+        $('[data-cell="7"]').text() === turn ){
+        alert(`${turn} Wins!`);
+    }
+        if($('[data-cell="2"]').text() === turn &&
+          $('[data-cell="5"]').text() === turn &&
+          $('[data-cell="8"]').text() === turn ){
+          alert(`${turn} Wins!`);
+      }
+          if($('[data-cell="0"]').text() === turn &&
+            $('[data-cell="4"]').text() === turn &&
+            $('[data-cell="8"]').text() === turn ){
+            alert(`${turn} Wins!`);
+        }
+            if($('[data-cell="2"]').text() === turn &&
+              $('[data-cell="4"]').text() === turn &&
+              $('[data-cell="6"]').text() === turn ){
+              alert(`${turn} Wins!`);
+          }
+              if($('[data-cell="0"]').text() === turn &&
+                $('[data-cell="1"]').text() === turn &&
+                $('[data-cell="2"]').text() === turn ){
+                alert(`${turn} Wins!`);
+            }
+                if($('[data-cell="3"]').text() === turn &&
+                  $('[data-cell="4"]').text() === turn &&
+                  $('[data-cell="5"]').text() === turn ){
+                  alert(`${turn} Wins!`);
+              }
+                  if($('[data-cell="6"]').text() === turn &&
+                    $('[data-cell="7"]').text() === turn &&
+                    $('[data-cell="8"]').text() === turn ){
+                    alert(`${turn} Wins!`);
+                }
+}
 });
