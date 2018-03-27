53e7c5875ac3ab042339edb1e262ee9d25b60d86
Tim <timcheek99@gmail.com>
2018-03-26 23:34:06 -0500

    built the beginnings of towers of hanoi game

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,38 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
+  // start of making data blocks draggable...
+    $('[data-block]').draggable({
+      revert: "invalid"
+    });
+
+  // ...End of making data blocks draggable
+
+  // Start of making drop zones droppable....
+
+    $('[data-stack]').droppable({
+      drop: function(event, ui){
+        var drag = $(ui.draggable).data('block');
+        console.log(drag);
+        var last = $(this).children().last().data('block');
+        console.log(last);
+        if(drag > last){
+          $(ui.draggable).draggable("option", "revert", true);
+        } else {
+          $(ui.draggable).appendTo($(this)).attr('style', 'position: relative');
+        }
+        // $(ui.draggable).appendTo($(this)).attr('style', 'position: relative');
+      }
+    })
+
+  //.....End of making drop zones droppable
+
+   //
 });
+
+// check for win
+function checkWin1(){
+  if($('[data-stack="3"]').children().length === 4){
+    $("#announce-game-won").text('WINNER WINNER');
+  }
+}
